3317073516f97a685b8ab73fedafa54d798f6d28
ngodonghai1998 <haiscar17@gmail.com>
2023-06-11 19:02:51 +0700

    them data tinh va giao dien home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,39 @@
 //rafce
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <div>Home</div>
-   )
- }
- 
- export default Home
+// import React from 'react'
+
+// const Home = () => {
+// 	return (
+// 		<div className='container'>
+
+// 		</div>
+// 	)
+// }
+
+// export default Home
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+//Thêm thư viện local để test hiển thị.
+// import { arrProduct } from '../assets/data/data'
+
+export class Home extends Component {
+	render() {
+		return (
+			<div className='container'>
+				<div className='d-flex'>
+					<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." />
+					<div>
+						<h3>Product name</h3>
+						<p>Product description</p>
+						<button className='btn btn-warning mx-2'>Buy now</button>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = (state) => ({})
+
+export default connect(mapStateToProps)(Home)
