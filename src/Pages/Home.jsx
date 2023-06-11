9546848b455ab8cbb592a4b19e1fcb1c188895fd
ngodonghai1998 <haiscar17@gmail.com>
2023-06-12 00:04:02 +0700

    anh them index scss va Home jsx mai anh tiep tuc nhe, co gi anh noi thay cho. Truoc cung tre nhieu lam

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,6 +29,71 @@ export class Home extends Component {
 						<button className='btn btn-warning mx-2'>Buy now</button>
 					</div>
 				</div>
+
+				<div className='custom_feature w-50 text-white'>
+					<h3>Product Feature</h3>
+				</div>
+				<div className='row'>
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+
+					<div className='col-3  custom_shoes mx-2 my-2'>
+						<img src="https://svcy3.myclass.vn/images/adidas-prophere-black-white.png" alt="..." className='w-100' />
+						<h4>Adidas Prophere</h4>
+						<p>short descript...</p>
+						<div className='d-flex'>
+							<button className='btn btn-success'>Buy now</button>
+							<p> 85 $</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
